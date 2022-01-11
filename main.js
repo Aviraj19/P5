80f3f15e7ca29dca0519a71fba4d51711a0417fe
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-01-11 15:27:41 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function preload() {
 
 function setup() {
   canvas=createCanvas(500,400);
-  canvas.position (100,200);
-  video=createCapture(VIDEO);
+  canvas.position(100,200);
+  video = createCapture(VIDEO);
   video.hide();
 }
  
 function draw() {
 image(video,0,0,500,400);
-tint(color)
+tint(colors)
 }
 function take_snapshot() {
     save("myphoto.png")
